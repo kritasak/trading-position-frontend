eab088acf554b292949511d67fa88a63125688dd
kritasak <jojokritasak@gmail.com>
2022-12-29 23:03:40 +0700

    Fix: Home Style

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -14,8 +14,12 @@ export default function Home() {
     return (
         <div className="home-wrapper">
             <h1>Trading Position Management</h1>
-            <button onClick={navigateToLogin}>Log In</button>
-            <button onClick={navigateToSignup}>Sign Up</button>
+            <button className="logInButton" onClick={navigateToLogin}>
+                Log In
+            </button>
+            <button className="signUpButton" onClick={navigateToSignup}>
+                Sign Up
+            </button>
         </div>
     );
 }
