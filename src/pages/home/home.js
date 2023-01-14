2262cdb6422647e413395ff3ff2c3540075813b7
jinniphabh <jinniphabh@gmail.com>
2023-01-14 21:05:36 +0700

    add pic

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -13,7 +13,10 @@ export default function Home() {
 
     return (
         <div className="home-wrapper">
-            <h1>Trading Position Management</h1>
+            <div className="pic">
+                <img src="https://lh3.googleusercontent.com/xyhqSXZzpngB1xgolCHlmZE42u7Bp847BbBSdpMpwMrFESKAJycVLX5h3ToatOnQzbyU7LGbc-I8k4-zw1XiId0Q_Hmo3E4UetdjAcZPkoyl5szUmUQXD4QYV2ml720oga0RtF3Pnw=w600-h315-p-k" />
+            </div>
+
             <button className="logInButton" onClick={navigateToLogin}>
                 Log In
             </button>
